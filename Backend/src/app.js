9e018757fe489ba2be43fe49688b8145b087d0de
Victorio Paskevicius <pskvictorio@gmail.com>
2026-01-07 14:02:01 -0300

    Creando archivo principal de backend app.js y conectando base de datos

---
+++ Backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+// Configuración de entorno
+dotenv.config()
+
+const app = express()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// Rutas
+import routerUsers from "./routes/users.route.js"
+app.use("/api/users", routerUsers)
+
+// Puerto
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`)
+})
---
+++ Backend/src/database/scriptDB.js
@@ -0,0 +1,47 @@
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const conn = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
+
+const initQuery = `
+    CREATE DATABASE IF NOT EXISTS minigame_db;
+    USE minigame_db;
+
+    CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password_hash VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    
+    CREATE TABLE IF NOT EXISTS scores (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT NOT NULL,
+        score INT NOT NULL,
+        incorrect_clicks INT NOT NULL,
+        duration INT COMMENT 'Duración de la partida en segundos',
+        played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    );
+    `;
+
+conn.connect((err) => {
+  if (err) {
+    return console.log("Error de conexion", err);
+  }
+  conn.query(initQuery, (err, results) => {
+    if (err) {
+      console.log("Error ejecutando script SQL", err);
+    }
+    console.log("✅ Base de datos y tablas creadas correctamente");
+    process.exit(1);
+  });
+});
