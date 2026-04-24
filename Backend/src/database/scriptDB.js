fdbdd214da59797eea48a6541b9902498db891ed
Victorio Paskevicius <pskvictorio@gmail.com>
2026-04-24 08:34:41 -0300

    Corrigiendo url de base de datos

--- Backend/src/database/scriptDB.js
+++ Backend/src/database/scriptDB.js
@@ -1,5 +1,3 @@
-import db from "./db.js";
-
 const initQuery = `
     CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
@@ -20,35 +18,3 @@ CREATE TABLE IF NOT EXISTS scores (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
     `;
-
-
-    const initDB = async () => {
-  try {
-    await db.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(50) NOT NULL UNIQUE,
-        password_hash TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
-    `);
-
-    await db.query(`
-      CREATE TABLE IF NOT EXISTS scores (
-        id SERIAL PRIMARY KEY,
-        user_id INTEGER NOT NULL,
-        username VARCHAR(50) NOT NULL,
-        score INTEGER NOT NULL,
-        incorrect_clicks INTEGER NOT NULL,
-        duration INTEGER,
-        level INTEGER NOT NULL,
-        played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-      );
-    `);
-
-    console.log("Tablas creadas correctamente");
-  } catch (error) {
-    console.error("Error creando tablas", error);
-  }
-};
